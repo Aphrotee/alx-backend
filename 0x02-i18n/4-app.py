38788e240b9a47b8aaa52b85846ef3bba965a5b8
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-02-01 16:16:18 +0100

    working on translation

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -30,6 +30,7 @@ def get_locale() -> str:
     """ Gets the language to be used """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
+        print(locale)
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
