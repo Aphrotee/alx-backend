0e9c9a3271dc7aadff12b60cac86217a6ee692e0
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-02-01 15:02:08 +0100

    initialize translation

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -11,18 +11,23 @@ from flask import (
 )
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
 
-
-class Config(object):
+class Config:
     """ A Config class for configuration """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ Gets the language to be used """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
@@ -30,15 +35,9 @@ def indexHtml() -> str:
     """ Creates html template """
     page_title = 'Welcome to Holberton'
     content = 'Hello world'
-    return render_template('2-index.html',
-                           page_title=page_title,
-                           content=content)
-
-
-@babel.localselector
-def get_locale() -> str:
-    """ Gets the language to be used """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return render_template('3-index.html',
+                           home_title=page_title,
+                           home_header=content)
 
 
 if __name__ == '__main__':
