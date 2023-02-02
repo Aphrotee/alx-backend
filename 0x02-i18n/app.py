ba9314dd4099141691238e3b7cb632b5d17bc048
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-02-02 00:57:43 +0000

    datetime display added;

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -67,7 +67,7 @@ def get_user() -> str:
 def get_timezone() -> str:
     """ Get the timezone to be used """
     tz = request.args.get('timezone')
-    print(format_datetime)
+
     if tz is None:
         try:
             tz = timezone(tz)
@@ -90,6 +90,7 @@ def before_request() -> None:
     """ Sets the global user as the logged in user """
     user = get_user()
     g.user = user
+    g.time = format_datetime()
 
 
 @app.route('/', strict_slashes=False)
@@ -98,12 +99,16 @@ def indexHtml() -> str:
     user = g.user
     login_status = 'not_logged_in'
     username = ''
+    time = g.time
+    time_str = 'current_time_is'
     if user is not None:
         login_status = 'logged_in_as'
         username = user.get('name')
-    return render_template('6-index.html',
+    return render_template('index.html',
                            login_status=login_status,
-                           username=username)
+                           username=username,
+                           current_time=time,
+                           time_str=time_str)
 
 
 if __name__ == '__main__':
